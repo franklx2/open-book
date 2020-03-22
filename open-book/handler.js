51371d93e98e27c314c64b8f698b0c847dcf9737
franklx2 <linfranklx2@gmail.com>
2020-03-21 23:20:24 -0700

    lambda can receive post request and extract data

--- open-book/handler.js
+++ open-book/handler.js
@@ -1,12 +1,20 @@
 'use strict';
+// const querystring = require('querystring');
 
 module.exports.hello = async event => {
+  // Parse the post body
+  // const data = querystring.parse(event.body);
+  const data = JSON.parse(event.body); // should wrap in try/catch
+  const text = data.text; // 'world'
+  // Access variables from body
+  // const text = data.text;
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        message: 'text: ' + text,
+        input: event.body,
       },
       null,
       2
