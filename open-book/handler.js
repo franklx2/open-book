3297260e9c55caaf1c8dffdfd63ca9d73b568052
franklx2 <linfranklx2@gmail.com>
2020-03-22 07:59:00 -0700

    able to upload files to s3 via api posts now. there is a bug in retrieving the file ID after it is created. will have to rexamine

--- open-book/handler.js
+++ open-book/handler.js
@@ -1,26 +1,82 @@
 'use strict';
-// const querystring = require('querystring');
+const ID_LENGTH = 10;
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
+var errors = [];
+var data = "";
+var text = "";
+var s3_status = "";
+var s3KeyName = "";
 
-module.exports.hello = async event => {
-  // Parse the post body
-  // const data = querystring.parse(event.body);
-  const data = JSON.parse(event.body); // should wrap in try/catch
-  const text = data.text; // 'world'
-  // Access variables from body
-  // const text = data.text;
+/*
+ * @TODO: For whatever reason I am not able to get the key name back on the response. But at least the file is being uploaded now. Thank God.
+ */
 
-  return {
+module.exports.hello = (event, context, callback) => {
+  init(event);
+  uploadToS3(text);
+  callback(null, {
     statusCode: 200,
     body: JSON.stringify(
       {
         message: 'text: ' + text,
         input: event.body,
+        errors: errors,
+        s3_status: s3_status,
+        s3KeyName: s3KeyName
       },
       null,
       2
     ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  })
 };
+
+function init(event) {
+  // Parse the post body and extract text field
+  try {
+    data = JSON.parse(event.body); 
+    text = data.text; 
+  } catch (error) {
+    errors.push(error.message);
+    return false;
+  }
+  
+}
+
+function uploadToS3(text) {
+  var bucketName = process.env.bucketName;
+  var keyName = makeId(ID_LENGTH)
+  var content = text;
+  var contentType = 'STRING_VALUE';
+  var acl = "public-read";
+  var params = { 
+    Bucket: bucketName, 
+    Key: keyName, 
+    Body: content, 
+    ContentType: contentType, 
+    ACL: acl
+  };
+  console.log("Upload to S3 in progress");
+  console.log(params);
+  s3.putObject(params, function (err, data) {
+    if(err) {
+      errors.push(err);
+      s3_status = "Failed to save object to " + bucketName + "/" + keyName;
+    }
+    else {
+      s3_status = "Successfully saved object to " + bucketName + "/" + keyName;
+      s3KeyName = keyName;
+    }
+  });
+}
+
+function makeId(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
---
+++ open-book/test.js
@@ -0,0 +1,26 @@
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
+
+var bucketName = "open-book-pages";
+var keyName = makeId(10);
+var content = 'This is a sample text file';
+
+var params = { Bucket: bucketName, Key: keyName, Body: content };
+console.log("Params: " + params);
+
+s3.putObject(params, function (err, data) {
+    if (err)
+        console.log(err)
+    else
+        console.log("Successfully saved object to " + bucketName + "/" + keyName);
+});
+
+function makeId(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
