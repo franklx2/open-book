925ec636e8fbc6b2c44c5a19e110ec3431564810
franklx2 <linfranklx2@gmail.com>
2020-03-23 01:34:36 -0700

    completed /create, /update functions. configured sls deploy to sync /spa director to open-book-spa bucket. also configured deployment to upload the INITPAGE (contains intro) to the open-book-pages bucket

--- open-book/handler.js
+++ open-book/handler.js
@@ -6,67 +6,100 @@ var errors = [];
 var data = "";
 var text = "";
 var s3_status = "";
-var keyName = makeId(ID_LENGTH)
+var keyName = "";
 var bucketName = process.env.bucketName;
-var content = text;
+var content = "";
 var contentType = 'STRING_VALUE';
 var acl = "public-read";
-var params = { 
-  Bucket: bucketName, 
-  Key: keyName, 
-  Body: content, 
-  ContentType: contentType, 
-  ACL: acl
-};
+
 
 /*
- * @TODO: The function is now able to return the keyName of the file that is created. We can use this to pass into another 
- * API call that will extract data from the file with the given keyName and pump it into a DynamoDB table. The next step is 
- * to refactor this function so it's not called "hello" :)
+ * @TODO: As of now I still need to complete the following:
+ * 0. Create a DynamoDB table that will do what you want... just an Int increment column, and a Text column
+ * 1. /publish function that will read a file from S3 and write the data to DynamoDB
+ * 2. Create a plugin that uploads a file to S3, pulls data with Lambda and writes to DynamoDB
+ *    - This will be a QA plugin that satisfies the plugin requirement of the exercise
+ * 3. Create The frontend page that displays the Open Book and also allows users to submit forms for editing/publishing
  */
 
-module.exports.hello = (event, context, callback) => {
-  init(event);
-  uploadToS3(text);
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'text: ' + text,
-        input: event.body,
-        errors: errors,
-        s3_status: s3_status,
-        s3KeyName: keyName
-      },
-      null,
-      2
-    ),
-  })
+// Creates new file and uploads to S3 with randomized key name
+module.exports.create = (event, context, callback) => {
+  createInit(event);
+  uploadToS3(text, callback);
 };
 
-function init(event) {
+// Creates new file and uploads to S3 with an existing key name (performs overwrite)
+module.exports.update = (event, context, callback) => {
+  updateInit(event);
+  uploadToS3(text, callback);
+};
+
+function updateInit(event) {
   // Parse the post body and extract text field
+  // and keyName field
   try {
     data = JSON.parse(event.body); 
     text = data.text; 
+    keyName = data.keyName;
   } catch (error) {
     errors.push(error.message);
     return false;
   }
-  
 }
 
-function uploadToS3(text) {
+function createInit(event) {
+  // Parse the post body and extract text field
+  try {
+    data = JSON.parse(event.body); 
+    text = data.text; 
+    keyName = makeId(ID_LENGTH);
+  } catch (error) {
+    errors.push(error.message);
+    return false;
+  }
+}
+
+function uploadToS3(text, callback) {
+  if(text!="")
+  {
+    content = text;
+  }
+  else
+  {
+    errors.push("ERROR. CONTENT EMPTY.");
+  }
+
+  var params = { 
+    Bucket: bucketName, 
+    Key: keyName, 
+    Body: content, 
+    ContentType: contentType, 
+    ACL: acl
+  };
+
   console.log("Upload to S3 in progress");
   console.log(params);
+
   s3.putObject(params, function (err, data) {
     if(err) {
       errors.push(err);
-      s3_status = "Failed to save object to " + bucketName + "/" + keyName;
+      console.log("Failed to save object to " + bucketName + "/" + keyName);
     }
     else {
-      s3_status = "Successfully saved object to " + bucketName + "/" + keyName;
-      s3KeyName = keyName;
+      console.log("Successfully saved object to " + bucketName + "/" + keyName);
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(
+          {
+            message: 'text: ' + text,
+            errors: errors,
+            s3KeyName: keyName,
+            status: "success"
+          },
+          null,
+          2
+        ),
+      });
     }
   });
 }
