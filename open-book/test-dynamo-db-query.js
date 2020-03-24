ab505dc3fc38c231c76b649f7c28f3a06f9db9fb
franklx2 <linfranklx2@gmail.com>
2020-03-24 05:55:47 -0700

    created a test node.js script that queries dynamodb. will need to modify this later for the lambda function

--- open-book/test-dynamo-db-query.js
+++ open-book/test-dynamo-db-query.js
@@ -7,16 +7,11 @@ AWS.config.update({
 var docClient = new AWS.DynamoDB.DocumentClient();
 
 var params = {
-    "TableName": "open-book-pages",
-    "KeyConditionExpression": "#ID = :ID ",
-    "ExpressionAttributeNames": {
-        ":userid": "ID"
-    },
-    "ExpressionAttributeValues": {
-        ":userid": {
-            "S": "manaf1"
-        }
-    }
+    "TableName": "open-book-pages-ddb",
+    "IndexName": "OBGSI",
+    "KeyConditionExpression": "#ID = :ID",
+    "ExpressionAttributeNames": {"#ID": "ID"},
+    "ExpressionAttributeValues": {":ID":  "1"}
 };
 
 docClient.query(params, function (err, data) {
@@ -24,5 +19,8 @@ docClient.query(params, function (err, data) {
         console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
     } else {
         console.log("Query succeeded.");
+        data.Items.forEach(function(item) {
+            console.log(" -", item.PAGE_NUM + ": " + item.TEXT);
+        });
     }
 });
